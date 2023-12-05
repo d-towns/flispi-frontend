27692e800207b0cbe6fe447d48227041f26b470a
dt0wns <dennis@dennistowns.com>
2023-12-05 17:50:26 -0500

    ddot

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -20,5 +20,6 @@ export const parseImages = (images: string) : string[] => {
   }
 
 export const getEnvionmentApiUrl = () => {
+    dotenv.config()
     return process.env.REACT_APP_NODE_ENV === 'development' ? process.env.REACT_APP_API_URL : process.env.REACT_APP_PROD_API_URL
 }
