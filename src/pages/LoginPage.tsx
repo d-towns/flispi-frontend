8511c723e5d72983406a062c9360a9b4966593fa
dt0wns <dennis@dennistowns.com>
2024-01-11 20:50:47 -0500

    better login again

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,8 +3,6 @@ import { useNavigate } from 'react-router-dom'
 import { useAuth0 } from '@auth0/auth0-react'
 
 const LoginPage = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
   const { loginWithRedirect } = useAuth0();
 
   const handleLogin = async () => {
@@ -45,15 +43,6 @@ const LoginPage = () => {
           <p className="mb-6 text-xs font-semibold leading-7 lg:w-3/4 text-grey-900">
             **Disclaimer: Flint Propterty Search is in no way affiliated with the Genesee County Land Bank. This is a independent project for the purpose of showcasing the properties available for purchase in the City of Flint.**
           </p>
-          {/* <div className="flex items-center gap-4 flex-row ">
-            <Link to="/search" className="flex items-center md:py-4 py-3 px-5 text-xs sm:text-sm font-bold text-white bg-gradient-to-br from-[#8ba2be] to-[#A9A9A9] focus:ring-4 shadow-lg hover:scale-105 focus:bg-purple-100 transition duration-300 rounded-xl">Find Properties</Link>
-            <Link to='/contact' className="flex items-center py-4 text-xs sm:text-sm font-medium px-7 text-dark-grey-700 hover:text-dark-grey-900 transition duration-300 rounded-2xl">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 mr-2">
-                <path fillRule="evenodd" d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z" clipRule="evenodd"></path>
-              </svg>
-              Contact Us
-            </Link>
-          </div> */}
       </div>
       <div className="block max-md:hidden my-6 border-l border-gray-300 h-full w-[1px]" />
 
@@ -72,7 +61,7 @@ const LoginPage = () => {
               px-4 py-3 mt-6">Log In with Email</button>
           </div>
           <hr className="my-6 border-gray-300 w-full" />
-          <button onClick={handleLogin} className="w-full block bg-[#8ba2be] hover:bg-gray-500 transition ease-in-out duration-200 text-white font-semibold rounded-lg
+          <button onClick={handleSignUp} className="w-full block bg-[#8ba2be] hover:bg-gray-500 transition ease-in-out duration-200 text-white font-semibold rounded-lg
               px-4 py-3 mt-6">Sign Up
           </button>
 
