194a28269742d4f56c3a951faef03e66e3ded0b7
dt0wns <dennis@dennistowns.com>
2024-01-15 00:22:22 -0500

    fix zipcodes call

--- src/services/property.service.ts
+++ src/services/property.service.ts
@@ -39,7 +39,7 @@ export const fetchFeaturedProperties = async () => {
 };
 
 export const fetchZipCodes = async () => {
-    const response = await axios.get(getEnvionmentApiUrl() + '/zipcodes',{
+    const response = await axios.get(getEnvionmentApiUrl() + '/properties/zipcodes',{
         headers:{
             'Content-Type': 'application/json'
           },
