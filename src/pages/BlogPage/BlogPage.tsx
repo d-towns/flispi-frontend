3b6d999f3c00c7960bb9e60a38ed44b7e40a2266
dt0wns <dennis@dennistowns.com>
2024-01-23 16:38:31 -0500

    console.log

--- src/pages/BlogPage/BlogPage.tsx
+++ src/pages/BlogPage/BlogPage.tsx
@@ -10,10 +10,7 @@ const BlogPage = () => {
   const [blog, setBlog] = useState<Blog>()
   const { slug } = useParams<{ slug: string }>()
 
-
   const fetchBlog = useCallback(async () => {
-    console.log(slug);
-
     const response = await fetch(getEnvionmentApiUrl() + `/blog/${slug}`)
     const data = await response.json()
     setBlog(data)
