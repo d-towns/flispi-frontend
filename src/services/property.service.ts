17fff27eaf0d949c8c1c3b99df7adee79e777984
dt0wns <dennis@dennistowns.com>
2024-01-29 12:08:30 -0500

    fix property fetch calc

--- src/services/property.service.ts
+++ src/services/property.service.ts
@@ -17,7 +17,9 @@ interface PropertyApiResponse {
 
 export const fetchPropertySearchData = async (searchParams: any, pageNumber: number, pageSize: number) : Promise<PropertyApiResponse> => {
     // Calculate the offset
-    const offset = pageNumber === 0 ? 0 : (pageNumber - 1) * pageSize;
+    console.log(pageNumber, pageSize);
+    
+    const offset = pageNumber === 0 ? 0 : (pageNumber) * pageSize;
 
 
     const response = await axios.get<PropertyApiResponse>(getEnvionmentApiUrl() + '/properties', {
