43b867f1350bbd1b40774af106a20fd4b8e92342
dt0wns <dennis@dennistowns.com>
2024-02-07 12:50:36 -0500

    thumbnail

--- src/components/ImageGallery/index.tsx
+++ src/components/ImageGallery/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Lightbox from "yet-another-react-lightbox";
 import Zoom from "yet-another-react-lightbox/plugins/zoom"
 import "yet-another-react-lightbox/styles.css";
+import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 
 interface ImageGalleryProps {
     images: string[];
@@ -11,6 +12,8 @@ interface ImageGalleryProps {
 const ImageGallery = ({ images } : ImageGalleryProps) => {
   const [currentImage, setCurrentImage] = useState(images[0]);
   const [lightboxOpen, setLightboxOpen] = useState(false)
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [visibleImages] = useState(5); 
   const animationDuration = 300;
   const maxZoomPixelRatio = 2;
   const zoomInMultiplier = 2;
@@ -22,7 +25,21 @@ const ImageGallery = ({ images } : ImageGalleryProps) => {
   const pinchZoomDistanceFactor = 100;
   const scrollToZoom = false;
   
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => {
+      let newSlide = prevSlide + 1;
+      if (newSlide > images.length - visibleImages) newSlide = images.length - visibleImages;
+      return newSlide;
+    });
+  };
 
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => {
+      let newSlide = prevSlide - 1;
+      if (newSlide < 0) newSlide = 0;
+      return newSlide;
+    });
+  };
 
   
   return (
@@ -35,19 +52,36 @@ const ImageGallery = ({ images } : ImageGalleryProps) => {
           className="w-full h-auto object-cover rounded-lg"
         />
       </div>
+      <div className="flex flex-col items-center">
       <div className="flex justify-center py-3 gap-2">
-        {images.map((image, index) => (
-          <div className='h-fit hover:scale-110 transition ease-in-out duration-200'>
-          <img
-            key={index}
-            src={image}
-            alt={`Thumbnail ${index + 1}`}
-            className="w-20 h-20 object-cover rounded cursor-pointer"
-            onClick={() => setCurrentImage(image)}
-          />
+        { images.length >= 5 &&
+      <button onClick={prevSlide} className=" p-2 rounded">
+          {/* Replace with a left arrow icon from Heroicons */}
+          <ArrowLeftIcon className="w-6 h-6" />
+        </button>
+}
+
+        {images.slice(currentSlide, currentSlide + visibleImages).map((image, index) => (
+          <div key={index} className='h-fit hover:scale-110 transition ease-in-out duration-200'>
+            <img
+              src={image}
+              alt={`Thumbnail ${index + 1}`}
+              className="w-20 h-20 object-cover rounded cursor-pointer"
+              onClick={() => setCurrentImage(image)}
+            />
           </div>
         ))}
+        {images.length >= 5 &&  <button onClick={nextSlide} className=" p-2 rounded">
+          {/* Replace with a right arrow icon from Heroicons */}
+          <ArrowRightIcon className="w-6 h-6" />
+        </button>}
+               
       </div>
+
+      <div className="flex justify-between w-full px-4">
+        
+      </div>
+    </div>
       <Lightbox
         open={lightboxOpen}
         close={() => setLightboxOpen(false)}
--- src/pages/PropertyDetailsPage.tsx
+++ src/pages/PropertyDetailsPage.tsx
@@ -84,7 +84,7 @@ const PropertyDetailsPage = () => {
 
   return (
     <div>
-      <div className=" xxl:w-3/4  mt-10 xxl:mx-auto px-16">
+      <div className=" xxl:w-3/4  mt-10 xxl:mx-auto px-10">
         <div className="grid xl:grid-cols-3 xl:grid-rows-1 grid-cols-1 grid-rows-auto gap-2 col-span-3 my-4 h-fit">
           <div className="lg:col-span-2 col-span-1 h-fit sm:h-full bg-white rounded-lg relative overflow-hidden my-10 sm:my-0">
             {property?.images ? <ImageGallery images={parseImages(property.images)} /> :
@@ -111,18 +111,18 @@ const PropertyDetailsPage = () => {
               </div>
               <div className="flex w-full md:flex-row flex-col border-t border-b borger-gray-100 px-5 justify-between py-6 font-sans md:text-lg text-base">
                 <div className="flex flex-col ">
-                  <span className="max-md:flex max-md:w-2/3 max-md:justify-between mb-1"> <span className="font-bold">Tax ID:</span> {property?.parcel_id}</span>
-                  <span className="max-md:flex max-md:w-2/3 max-md:justify-between mb-1"><span className="font-bold">Property Class: </span> {property?.property_class}</span>
-                  <span className="max-md:flex max-md:w-2/3 max-md:justify-between mb-1"><span className="font-bold">Price:</span> {property?.price ? currencyFormat.format(property.price) : 'Price Negotiable'}</span>
-                  {!property?.property_class.includes('Lot') && <span className="max-md:flex max-md:w-2/3 max-md:justify-between"><span className="font-bold">Square Feet:</span> {property?.square_feet}</span>}
+                  <span className="max-md:flex max-md:w-2/3 max-sm:w-full max-md:justify-between mb-1"> <span className="font-bold">Tax ID:</span> {property?.parcel_id}</span>
+                  <span className="max-md:flex max-md:w-2/3 max-sm:w-full  max-md:justify-between mb-1"><span className="font-bold">Property Class: </span> {property?.property_class}</span>
+                  <span className="max-md:flex max-md:w-2/3  max-sm:w-full max-md:justify-between mb-1"><span className="font-bold">Price:</span> {property?.price ? currencyFormat.format(property.price) : 'Price Negotiable'}</span>
+                  {!property?.property_class.includes('Lot') && <span className="max-md:flex max-sm:w-full  max-md:w-2/3 max-md:justify-between"><span className="font-bold">Square Feet:</span> {property?.square_feet}</span>}
                 </div>
                 <div className="flex flex-col">
-                  <span className="max-md:flex max-md:w-2/3 max-md:justify-between mb-1"><span className="font-bold">Lot Size:</span> {property?.lot_size ?? 'N/A'}</span>
-                  <span className="max-md:flex max-md:w-2/3 max-md:justify-between mb-1"><span className="font-bold">Year Built:</span> {property?.year_built ?? 'N/A'}</span>
+                  <span className="max-md:flex max-md:w-2/3  max-sm:w-full max-sm:w-full  max-md:justify-between mb-1"><span className="font-bold">Lot Size:</span> {property?.lot_size ?? 'N/A'}</span>
+                  <span className="max-md:flex max-md:w-2/3 max-sm:w-full  max-md:justify-between mb-1"><span className="font-bold">Year Built:</span> {property?.year_built ?? 'N/A'}</span>
                   { !property?.property_class.includes('Lot') &&
                   
-                ( <> <span className="max-md:flex max-md:w-2/3 max-md:justify-between mb-1"><span className="font-bold">Bedrooms:</span> {property?.bedrooms ?? 'N/A'}</span>
-                  <span className="max-md:flex max-md:w-2/3 max-md:justify-between mb-1"><span className="font-bold">Bathrooms:</span> {property?.bathrooms ?? 'N/A'}</span></>
+                ( <> <span className="max-md:flex max-md:w-2/3 max-sm:w-full max-md:justify-between mb-1"><span className="font-bold">Bedrooms:</span> {property?.bedrooms ?? 'N/A'}</span>
+                  <span className="max-md:flex max-md:w-2/3 max-sm:w-full  max-md:justify-between mb-1"><span className="font-bold">Bathrooms:</span> {property?.bathrooms ?? 'N/A'}</span></>
 )}
                 </div>
               </div>
