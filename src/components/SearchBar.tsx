d42fdeb094025b55905df59a197dbe3fc94aaa53
dt0wns <dennis@dennistowns.com>
2024-02-11 21:39:41 -0500

    white bg search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -27,7 +27,7 @@ const SearchBar: FC<SearchBarProps> = ({ searchParams, setSearchParams }) => {
   return (
     <>
       <div className="pt-2 relative mx-auto text-gray-600 w-full px-2 flex flex-row items-center">
-        <input className="relative w-full cursor-default overflow-hidden rounded-lg text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-blue-300 sm:text-sm h-10 px-5 bg-gray-200 focus:bg-gray-100"
+        <input className="relative w-full cursor-default overflow-hidden rounded-lg text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-blue-300 sm:text-sm h-10 px-5 bg-white focus:bg-gray-100"
           type="search" name="search" placeholder="Search Address or Parcel ID #"
           value={searchTerm}
           onChange={(e) => handleChange(e)} />
