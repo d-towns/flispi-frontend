52cdabf771747b670ebb99e1c06ccc6d40cb923c
dt0wns <dennis@dennistowns.com>
2024-02-12 20:43:55 -0500

    better gallery

--- src/components/ImageGallery/index.tsx
+++ src/components/ImageGallery/index.tsx
@@ -10,7 +10,7 @@ interface ImageGalleryProps {
 
 
 const ImageGallery = ({ images } : ImageGalleryProps) => {
-  const [currentImage, setCurrentImage] = useState(images[0]);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [lightboxOpen, setLightboxOpen] = useState(false)
   const [currentSlide, setCurrentSlide] = useState(0);
   const [visibleImages] = useState(5); 
@@ -26,17 +26,38 @@ const ImageGallery = ({ images } : ImageGalleryProps) => {
   const scrollToZoom = false;
   
   const nextSlide = () => {
+    setCurrentImageIndex((currentIndexVal) => {
+      if (currentIndexVal < images.length - 1) return currentIndexVal + 1;
+      else return currentIndexVal
+    });
     setCurrentSlide((prevSlide) => {
-      let newSlide = prevSlide + 1;
-      if (newSlide > images.length - visibleImages) newSlide = images.length - visibleImages;
+      
+      
+      let newSlide = prevSlide
+      console.log('currentImageIndex', currentImageIndex);
+      console.log('prevSlide + vis', prevSlide + visibleImages);
+      
+      if(currentImageIndex === prevSlide + visibleImages - 1) return newSlide = prevSlide + 1;
+      if (newSlide > images.length - visibleImages) return newSlide = images.length - visibleImages;
       return newSlide;
     });
+
+
+    console.log('Next');
   };
 
   const prevSlide = () => {
+    setCurrentImageIndex((currentIndexVal) => {
+      if (currentIndexVal > 0) return currentIndexVal - 1;
+      else return currentIndexVal
+    });
     setCurrentSlide((prevSlide) => {
-      let newSlide = prevSlide - 1;
-      if (newSlide < 0) newSlide = 0;
+
+      let newSlide = prevSlide
+      console.log('currentImageIndex', currentImageIndex);
+      console.log('prevSlide + vis', prevSlide);
+      if(currentImageIndex === prevSlide) return newSlide = prevSlide - 1;
+      if (newSlide < 0) return newSlide = 0;
       return newSlide;
     });
   };
@@ -46,7 +67,7 @@ const ImageGallery = ({ images } : ImageGalleryProps) => {
     <div className="max-w-4xl mx-auto">
       <div className="mb-4">
         <img 
-          src={currentImage} 
+          src={images[currentImageIndex]} 
           onClick={ () => setLightboxOpen(true)}
           alt="Displayed"
           className="w-full h-auto object-cover rounded-lg"
@@ -55,24 +76,24 @@ const ImageGallery = ({ images } : ImageGalleryProps) => {
       <div className="flex flex-col items-center">
       <div className="flex justify-center py-3 gap-2">
         { images.length >= 5 &&
-      <button onClick={prevSlide} className=" p-2 rounded">
-          {/* Replace with a left arrow icon from Heroicons */}
+      <button onClick={prevSlide} disabled={currentImageIndex === 0}
+      className=" disabled:opacity-10 p-2 rounded">
           <ArrowLeftIcon className="w-6 h-6" />
         </button>
 }
 
-        {images.slice(currentSlide, currentSlide + visibleImages).map((image, index) => (
-          <div key={index} className='h-fit hover:scale-110 transition ease-in-out duration-200'>
+        {images.slice(currentSlide, currentSlide + visibleImages).map((image, index) => ( 
+          <div key={index} className={`h-fit transition ease-in-out duration-200 hover:scale-110 rounded-lg ${currentImageIndex === index && 'border-2 border-black '}`}>
             <img
               src={image}
               alt={`Thumbnail ${index + 1}`}
-              className="w-20 h-20 object-cover rounded cursor-pointer"
-              onClick={() => setCurrentImage(image)}
+              className="w-20 h-20 object-cover rounded cursor-pointe "
+              onClick={() => setCurrentImageIndex(index + currentSlide)}
             />
           </div>
         ))}
-        {images.length >= 5 &&  <button onClick={nextSlide} className=" p-2 rounded">
-          {/* Replace with a right arrow icon from Heroicons */}
+        {images.length >= 5 &&  <button onClick={nextSlide} disabled={currentImageIndex === images.length - 1}
+          className=" p-2 rounded disabled:opacity-10">
           <ArrowRightIcon className="w-6 h-6" />
         </button>}
                
