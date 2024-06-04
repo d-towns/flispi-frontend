ffeb955e30915653c8c52cd0c6def55c8b565368
dt0wns <dennis@dennistowns.com>
2024-06-04 13:43:22 -0400

    fix bottom grid pagination

--- src/components/FilterSection.tsx
+++ src/components/FilterSection.tsx
@@ -489,8 +489,16 @@ const FilterSection: FC = () => {
                       </div>
                       <div>
                         <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-                          <button
+                        <button
+                            onClick={() => setPageNumber(pageNumber + 1)}
+                            disabled={searchTotal < PAGE_SIZE}
                             className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                          >
+                            <span className="sr-only">Next</span>
+                            <ChevronDoubleLeftIcon className="h-5 w-5" aria-hidden="true" />
+                          </button>
+                          <button
+                            className="relative inline-flex items-center px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
                             onClick={() => setPageNumber(pageNumber - 1)}
                             disabled={pageNumber === 0}
                           >
@@ -502,7 +510,7 @@ const FilterSection: FC = () => {
                             return (
                               <button
                                 key={"pgn-" + number}
-                                onClick={() => setPageNumber(number)}
+                                onClick={() => setPageNumber(number - 1)}
                                 disabled={searchTotal < PAGE_SIZE}
                                 className="relative inline-flex items-center px-4 py-2 text-sm hover:bg-gray-200 font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0"
                                 style={
@@ -516,7 +524,7 @@ const FilterSection: FC = () => {
                           <button
                             onClick={() => setPageNumber(pageNumber + 1)}
                             disabled={searchTotal < PAGE_SIZE}
-                            className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                            className="relative inline-flex items-center px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
                           >
                             <span className="sr-only">Next</span>
                             <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
