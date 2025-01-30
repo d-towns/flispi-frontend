cbd038e2ee0b349464d54214dda708833def49e4
dt0wns <dennis@dennistowns.com>
2025-01-30 09:27:04 -0500

    update FilterSection to add padding for improved mobile layout

--- src/components/FilterSection.tsx
+++ src/components/FilterSection.tsx
@@ -76,7 +76,7 @@ const FilterSection: FC = () => {
                 leaveTo="translate-x-full"
               >
                 {/* Mobile sidebar header */}
-                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl">
+                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-12 shadow-xl px-2">
                   <div className="flex items-center justify-between px-4">
                     <div className=" ">
                       <h2 className="text-lg font-medium text-gray-900">Filters</h2>
@@ -92,7 +92,7 @@ const FilterSection: FC = () => {
                   </div>
 
                   {/* Mobile Filters */}
-                  <form className="mt-4 border-t border-gray-200">
+                  <form className="mt-4 border-t border-gray-200 px-2 ">
                     <div>
                       <h3 className="sr-only">Categories</h3>
                     </div>
